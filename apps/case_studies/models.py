e8e6bee4e621a88d7d9cf7be340465a7bbfac4f3
joshdels <assistantgisjosh@gmail.com>
2026-08-25 19:17:16 +0800

    feat: update python v 12 - 14, then wagtail cms

--- apps/case_studies/models.py
+++ apps/case_studies/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from wagtail.fields import RichTextField
+from wagtail.models import Page
 
-# Create your models here.
+
+class CaseStudyIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    parent_page_types = [
+        "wagtailcore.Page",
+    ]
+
+    subpage_types = [
+        "case_studies.CaseStudyPage",
+    ]
+
+    content_panels = Page.content_panels + [
+        "intro",
+    ]
+
+
+class CaseStudyPage(Page):
+    client = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, blank=True)
+    project_type = models.CharField(max_length=255, blank=True)
+
+    summary = models.TextField()
+    challenge = RichTextField()
+    solution = RichTextField()
+    results = RichTextField()
+
+    parent_page_types = [
+        "case_studies.CaseStudyIndexPage",
+    ]
+
+    content_panels = Page.content_panels + [
+        "client",
+        "location",
+        "project_type",
+        "summary",
+        "challenge",
+        "solution",
+        "results",
+    ]
--- config/settings/base.py
+++ config/settings/base.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     # Local
     "core",
     "apps.guests",
+    "apps.case_studies",
 ]
 # ============================================================
 # MIDDLEWARE
