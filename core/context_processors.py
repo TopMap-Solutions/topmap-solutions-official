c88ded5e6e532b77869bc128b98f88a31ca018da
joshdels <assistantgisjosh@gmail.com>
2026-08-27 00:53:50 +0800

    chore: cache nav route

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,7 +1,12 @@
+from django.core.cache import cache
 from apps.case_studies.models import CaseStudyIndexPage
 
 
 def navigation(request):
     return {
-        "case_studies": CaseStudyIndexPage.objects.live().first(),
+        "case_studies": cache.get_or_set(
+            "navigation_case_studies",
+            lambda: CaseStudyIndexPage.objects.live().first(),
+            60 * 60 * 24,
+        ),
     }
